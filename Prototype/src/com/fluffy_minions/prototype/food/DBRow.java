d59c10978b49380a3c37d3b62bbf29324795b654
Bianca <bianca.trif28@gmail.com>
2014-05-19 16:53:36 +0300

    Improved methods for algorithm

--- Prototype/src/com/fluffy_minions/prototype/food/DBRow.java
+++ Prototype/src/com/fluffy_minions/prototype/food/DBRow.java
@@ -4,8 +4,7 @@ package com.fluffy_minions.prototype.food;
  * Created by sorin on 5/19/14.
  */
 public class DBRow {
-    private final long id;
-    private final String name;
+	private final String name;
     private final int breakfast;
     private final int lunch;
     private final int dinner;
@@ -18,6 +17,55 @@ public class DBRow {
     private final int fibers;
     private final int vitamins;
     private final int minerals;
+    
+    private final long id;
+    public long getId() {
+		return id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public int getBreakfast() {
+		return breakfast;
+	}
+
+	public int getLunch() {
+		return lunch;
+	}
+
+	public int getDinner() {
+		return dinner;
+	}
+
+	public int getNeeds_sidedish() {
+		return needs_sidedish;
+	}
+
+	public int getIs_sidedish() {
+		return is_sidedish;
+	}
+
+	public int getCalories() {
+		return calories;
+	}
+
+	public int getProteins() {
+		return proteins;
+	}
+
+	public int getFats() {
+		return fats;
+	}
+
+	public int getCarbs() {
+		return carbs;
+	}
+
+	public int getFibers() {
+		return fibers;
+	}
 
     public DBRow(long id, String name, int breakfast, int lunch, int dinner, int needs_sidedish, int is_sidedish, int calories, int proteins, int fats, int carbs, int fibers, int vitamins, int minerals) {
         this.id = id;
@@ -35,4 +83,6 @@ public class DBRow {
         this.vitamins = vitamins;
         this.minerals = minerals;
     }
+    
+    
 }
--- Prototype/src/com/fluffy_minions/prototype/food/DBRows.java
+++ Prototype/src/com/fluffy_minions/prototype/food/DBRows.java
@@ -1,38 +1,73 @@
-package com.fluffy_minions.prototype.food;
+package food;
 
-import com.fluffy_minions.prototype.IDAS.IMeal;
-import com.fluffy_minions.prototype.needsCalculators.PersonalProfile;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+import IDAS.IMeal;
+import IDAS.TotalNeedsCalculator;
+import needsCalculators.PersonalProfile;
 
 /**
  * Created by sorin on 5/19/14.
  */
 public class DBRows implements IMeal {
-    private void add(DBRow row) {
-
+	private List<DBRow> rows = new ArrayList<DBRow>();
+	
+    public void add(DBRow row) {
+    	rows.add(row);
     }
 
     @Override
     public String[] getNames() {
-        return new String[0];
+    	List<String> names = new ArrayList<String>();
+        for (DBRow row:rows) {
+        	names.add(row.getName());
+        }
+		return names.toArray(new String[rows.size()]);
     }
 
     @Override
     public String[] getIngredients() {
-        return new String[0];
+        String[] ingredients = { "CALORIES" , "FATS" , "CARBS" , "FIBER", "PROTEINS" };
+        return ingredients;
     }
 
     @Override
     public int[] getMinimumRequiredIngredients(PersonalProfile profile) {
-        return new int[0];
+    	TotalNeedsCalculator calculator = new TotalNeedsCalculator();
+		calculator.computeNeeds();
+		return new int[] {(int)calculator.getBreakfastCalories(), (int)calculator.getBreakfastFats(),
+				(int)calculator.getBreakfastCarbs(), (int)calculator.getBreakfastFiber(), 
+				(int)calculator.getBreakfastProteins()};
     }
 
     @Override
     public int[] getPrices() {
-        return new int[0];
+    	Random random = new Random();
+        return new int[] { random.nextInt(50) + 1,  random.nextInt(50) + 1, random.nextInt(50) + 1, random.nextInt(50) + random.nextInt(50) + 1 };
     }
 
     @Override
     public int[][] getIngredientsMatrix() {
-        return new int[0][];
+    	int[][] ingredients;
+    	for(int i = 0;  i < this.getIngredients().length; i++) {
+    		for(int j = 0;  j < rows.size(); j++) {
+    			if(i == 0) {
+    				ingredients[i][j] = rows.get(j).getCalories();
+    			}else {
+    				if( i == 1) {
+    					ingredients[i][j] = rows.get(j).getFats();
+    				} else if( i == 2 ) {
+    					ingredients[i][j] = rows.get(j).getCarbs();
+    				} else if( i == 3) {
+    					ingredients[i][j] = rows.get(j).getFibers();
+    				} else {
+    					ingredients[i][j] = rows.get(j).getProteins();
+    				}
+    			}
+    		}
+    	}
+    	return ingredients;
     }
 }
