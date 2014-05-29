b098c1fda57d3e5734828aaac25d8235c2ff53ee
Bianca Trif <bianca.trif28@gmail.com>
2014-05-29 20:27:19 +0300

    Made the menu persistent

--- Prototype/src/com/fluffy_minions/prototype/Menu.java
+++ Prototype/src/com/fluffy_minions/prototype/Menu.java
@@ -2,6 +2,8 @@ package com.fluffy_minions.prototype;
 
 import android.app.Activity;
 import android.app.Fragment;
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -40,12 +42,17 @@ public class Menu extends SherlockFragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 
-        IMeal breakfastMeal = new Breakfast(mainActivity.getSqLiteHelper(), mainActivity.getPersonalProfile());
+        /*IMeal breakfastMeal = new Breakfast(mainActivity.getSqLiteHelper(), mainActivity.getPersonalProfile());
         IMeal lunchMeal = new Lunch(mainActivity.getSqLiteHelper(), mainActivity.getPersonalProfile());
         IMeal dinnerMeal = new Dinner(mainActivity.getSqLiteHelper(), mainActivity.getPersonalProfile());
 
         PersonalProfile personalProfile = mainActivity.getPersonalProfile();
-        JacopWizard jacopWizard = new JacopWizard();
+        JacopWizard jacopWizard = new JacopWizard();*/
+
+        Bundle args = getArguments();
+        int day = args.getInt("day");
+
+        SharedPreferences sharedPreferences = mainActivity.getSharedPreferences("menu", Context.MODE_PRIVATE);
 
 
         View view = inflater.inflate(R.layout.menu_fragment, container, false);
@@ -58,7 +65,7 @@ public class Menu extends SherlockFragment {
         spec1.setIndicator("Breakfast");
 
         TextView breakfast = (TextView) view.findViewById(R.id.breakfast_textView);
-        breakfast.setText(jacopWizard.invokeTheGods(breakfastMeal, personalProfile, LOGGER));
+        breakfast.setText(sharedPreferences.getString(String.valueOf(day) + "0", ""));
         //set(breakfastMeal, breakfast);
 
         TabHost.TabSpec spec2=tabHost.newTabSpec("Tab 2");
@@ -66,7 +73,8 @@ public class Menu extends SherlockFragment {
         spec2.setContent(R.id.tab2);
 
         TextView lunch = (TextView) view.findViewById(R.id.lunch_textView);
-        lunch.setText(jacopWizard.invokeTheGods(lunchMeal, personalProfile, LOGGER));
+        // lunch.setText(jacopWizard.invokeTheGods(lunchMeal, personalProfile, LOGGER));
+        lunch.setText(sharedPreferences.getString(String.valueOf(day) + "1", ""));
         // set(lunchMeal, lunch);
 
         TabHost.TabSpec spec3=tabHost.newTabSpec("Tab 3");
@@ -74,7 +82,8 @@ public class Menu extends SherlockFragment {
         spec3.setContent(R.id.tab3);
 
         TextView dinner = (TextView) view.findViewById(R.id.dinner_textView);
-        dinner.setText(jacopWizard.invokeTheGods(dinnerMeal, personalProfile, LOGGER));
+        // dinner.setText(jacopWizard.invokeTheGods(dinnerMeal, personalProfile, LOGGER));
+        dinner.setText(sharedPreferences.getString(String.valueOf(day) + "2", ""));
         // set(dinnerMeal, dinner);
 
         tabHost.addTab(spec1);
--- Prototype/src/com/fluffy_minions/prototype/ViewMenuFragment.java
+++ Prototype/src/com/fluffy_minions/prototype/ViewMenuFragment.java
@@ -2,17 +2,30 @@ package com.fluffy_minions.prototype;
 
 import android.app.Activity;
 import android.app.Fragment;
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.*;
 import android.widget.AdapterView;
 import android.widget.ListView;
 import android.widget.Toast;
 import com.actionbarsherlock.app.SherlockFragment;
+import com.actionbarsherlock.view.*;
+import com.actionbarsherlock.view.MenuInflater;
+import com.actionbarsherlock.view.MenuItem;
+import com.fluffy_minions.prototype.IDAS.Breakfast;
+import com.fluffy_minions.prototype.IDAS.Dinner;
+import com.fluffy_minions.prototype.IDAS.IMeal;
+import com.fluffy_minions.prototype.IDAS.Lunch;
+import com.fluffy_minions.prototype.needsCalculators.PersonalProfile;
+
+import java.util.logging.Logger;
 
 /**
  * Created by sorin on 3/24/14.
  */
 public class ViewMenuFragment extends SherlockFragment {
+    private static final Logger LOGGER = Logger.getLogger(ViewMenuFragment.class.getName());
     private MainActivity mMainActivity;
 
     @Override
@@ -26,13 +39,20 @@ public class ViewMenuFragment extends SherlockFragment {
         listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+
+
                 if(mMainActivity.getPersonalProfile() == null) {
                     Toast.makeText(mMainActivity, "Please fill out your profile", Toast.LENGTH_LONG).show();
                     return;
                 }
 
+                if(mMainActivity.getSharedPreferences("menu", Context.MODE_PRIVATE).getString("00", "").equals("")) {
+                    Toast.makeText(mMainActivity, "Please generate a menu", Toast.LENGTH_LONG).show();
+                    return;
+                }
+
                 Bundle args = new Bundle();
-                args.putString("day", (String)adapterView.getItemAtPosition(i));
+                args.putInt("day", i);
 
                  SherlockFragment fragment = new Menu();
                 fragment.setArguments(args);
@@ -49,6 +69,45 @@ public class ViewMenuFragment extends SherlockFragment {
         super.onAttach(activity);
 
         mMainActivity = (MainActivity) activity;
+    }
+
+    @Override
+    public void onCreateOptionsMenu(com.actionbarsherlock.view.Menu menu, MenuInflater inflater) {
+        super.onCreateOptionsMenu(menu, inflater);
+
+        inflater.inflate(R.menu.view_menu_menu, menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch(item.getItemId()) {
+            case R.id.regenerate_menu: {
+                IMeal[] meals = {
+                    new Breakfast(mMainActivity.getSqLiteHelper(), mMainActivity.getPersonalProfile()),
+                    new Lunch(mMainActivity.getSqLiteHelper(), mMainActivity.getPersonalProfile()),
+                    new Dinner(mMainActivity.getSqLiteHelper(), mMainActivity.getPersonalProfile())
+                };
+
+                PersonalProfile personalProfile = mMainActivity.getPersonalProfile();
+
+                JacopWizard jacopWizard = new JacopWizard();
+
+                SharedPreferences sharedPreferences = mMainActivity.getSharedPreferences("menu", Context.MODE_PRIVATE);
+                SharedPreferences.Editor editor = sharedPreferences.edit();
+
+                for(int day = 0; day < 7; ++day) {
+                    for(int meal = 0; meal < 3; ++meal) {
+                        String key = String.valueOf(day) + String.valueOf(meal);
+                        String value = jacopWizard.invokeTheGods(meals[meal], personalProfile, LOGGER);
+
+                        editor.putString(key, value);
+                    }
+                }
+
+                editor.commit();
+            }
+            default: { return super.onOptionsItemSelected(item); }
+        }
     }
 
     /*@Override
