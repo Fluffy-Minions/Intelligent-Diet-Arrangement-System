ce1168c1e9cd61fc787ef567e13217f0310f823b
andreea93m <m_andreea93@yahoo.com>
2014-06-02 14:13:39 +0300

    Added documentation for some classes.

--- Prototype/src/com/fluffy_minions/prototype/needsCalculators/TotalCaloriesCalculator.java
+++ Prototype/src/com/fluffy_minions/prototype/needsCalculators/TotalCaloriesCalculator.java
@@ -1,41 +1,92 @@
 package com.fluffy_minions.prototype.needsCalculators;
 
+/**
+ * This class stores and computes the necessary calories for a person in a day.
+ */
+
 public class TotalCaloriesCalculator {
-	private double bmr; //basal metabolic rate
-	private double bmi; // body mass index
+
+    /**
+     * The fields represent the basal metabolic rate,
+     * the body mass index, the necessary calories,
+     * the minimum necessary calories and the maximum number of calories
+     * in a day.
+     */
+
+	private double bmr;
+	private double bmi;
 	private double finalTDEE;
 	private double minCalories;
 	private double maxCalories;
+
+    /**
+     * This method returns the basal metabolic rate.
+     * @return bmr
+     */
 
 	public double getBmr() {
 		return bmr;
 	}
 
-	public void setBmr(double bmr) {
+    /**
+     * This method changes the value of the basal metabolic rate.
+     * @param bmr new value for the bmr
+     */
+
+    public void setBmr(double bmr) {
 		this.bmr = bmr;
 	}
+
+    /**
+     * This method returns the number of necessary calories.
+     * @return necessary calories
+     */
 
 	public double getTDEE(){
 		return finalTDEE;
 	}
 
-	public double getMinCalories() {
+    /**
+     * This method returns the number of minimum necessary calories.
+     * @return minimum necessary calories
+     */
+
+    public double getMinCalories() {
 		return minCalories;
 	}
 
-	public double getMaxCalories() {
+    /**
+     * This method returns the number of maximum calories.
+     * @return maximum calories
+     */
+
+    public double getMaxCalories() {
 		return maxCalories;
 	}
 
+    /**
+     * This method computes the bmr according to the gender, age, weight
+     * and height of the person.
+     * @param profile information about the person
+     * @return value of the bmr
+     */
+
 	public double computeBMR(PersonalProfile profile) {
 		if(profile.getGender().equals("Male")) {
-			bmr = (88.362 + (double)(13.397 * profile.getWeight()) + (double)(4.799 * profile.getHeight()) - (5.677 * profile.getAge()));
+			bmr = 88.362 + 13.397 * profile.getWeight() + 4.799 * profile.getHeight() - 5.677 * profile.getAge();
 		}
 		else {
-			bmr = (447.593 + (double)(9.247 * profile.getWeight()) + (double)(3.098 * profile.getHeight()) - (4.330 * profile.getAge()));
+			bmr = 447.593 + 9.247 * profile.getWeight() + 3.098 * profile.getHeight() - 4.330 * profile.getAge();
 		}
 		return bmr;
 	}
+
+    /**
+     * This method returns the number of calories for a day
+     * according to the bmr and the bmi.
+     * @param profile information about the person
+     * @return  average number of calories necessary in a day
+     */
 
 	public double computeTDEE(PersonalProfile profile)
 	{
@@ -68,17 +119,37 @@ public class TotalCaloriesCalculator {
 		return finalTDEE;
 	}
 
+    /**
+     * This method computes the body mass index for a person.
+     * @param profile information about the person
+     * @return  bmi
+     */
+
 	public double computeBMI(PersonalProfile profile) {
 		double height = profile.getHeight()/100;
 		bmi = profile.getWeight() / (height * height);
 		return bmi;
 	}
 
-	public double computeMinCalories(PersonalProfile profile) {
+    /**
+     * This method computes the minimum number of calories that can be eaten in a day,
+     * which is 10% less than the average number.
+     * @param profile information about the person
+     * @return        number of minimum calories
+     */
+
+    public double computeMinCalories(PersonalProfile profile) {
 		minCalories = 0.9 * this.computeTDEE(profile);
 		return minCalories;
 	}
-	
+
+    /**
+     * This method computes the maximum number of calories that can be eaten in a day,
+     * which is 10% more than the average number.
+     * @param profile information about the person
+     * @return        number of maximum calories
+     */
+
 	public double computeMaxCalories(PersonalProfile profile) {
 		maxCalories = 1.1 * this.computeTDEE(profile);
 		return maxCalories;
